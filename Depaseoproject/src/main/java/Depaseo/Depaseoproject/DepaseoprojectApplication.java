bd181e1cb157dcaddccb99dda1b7168e4e601a13
admin CIFO <admin CIFO@DESKTOP-K9FH0BE>
2021-07-15 13:50:47 +0200

    commit 1

---
+++ Depaseoproject/src/main/java/Depaseo/Depaseoproject/DepaseoprojectApplication.java
@@ -0,0 +1,13 @@
+package Depaseo.Depaseoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepaseoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepaseoprojectApplication.class, args);
+	}
+
+}
---
+++ Depaseoproject/src/test/java/Depaseo/Depaseoproject/DepaseoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package Depaseo.Depaseoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepaseoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
